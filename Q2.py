302bc4a029098765ac851b96613f4c47125e2ca9
rajpratik1606 <76464628+BlanK-1606@users.noreply.github.com>
2023-11-11 18:01:46 +0530

    updated_Q2.py

--- Q2.py
+++ Q2.py
@@ -5,6 +5,9 @@ from mininet.cli import CLI
 from mininet.node import OVSController
 from mininet.log import setLogLevel, info
 import subprocess
+import matplotlib.pyplot as plt
+import pandas as pd
+
 
 class MyTopo( Topo ):
     "My topology."
@@ -48,30 +51,27 @@ def runTopo():
     n = Mininet(topology)
     n.start()
 
+
+
     if args.config == 'a':
         server_process = n['h4'].popen(f"iperf -s -t 5 -i 0.5 -p 3000 > server_file_output.txt",shell=True)
         h1_clinet_process = n['h1'].popen(f"iperf -c 10.0.0.4 -t 5  -i 0.5 -p 3000 > client_h1.txt",shell=True)
         h1_clinet_process.wait()
 
+
     elif args.config == 'b':
-        #server_file_output = f"_{args.config}server_file_output.txt"
-        # print(server_file_output)
-        # print(f"iperf -s -t 5 -i 1 -p 3000 -Z {args.congestion} > {server_file_output}")
 
         server_process = n['h4'].popen(f"iperf -s -t 5 -i 0.5 -p 3000 > server_file_output.txt",shell=True)
-        h1_clinet_process_cubic = n['h1'].popen(f"iperf -c 10.0.0.4 -t 5  -i 0.5 -p 3000 -Z cubic > client_h1_cubic.txt",shell=True)
-        h1_clinet_process_reno = n['h1'].popen(f"iperf -c 10.0.0.4 -t 5  -i 0.5 -p 3000 -Z reno > client_h1_reno.txt",shell=True)
-        h1_clinet_process_bbr = n['h1'].popen(f"iperf -c 10.0.0.4 -t 5  -i 0.5 -p 3000 -Z bbr > client_h1_bbr.txt",shell=True)
-        h1_clinet_process_vegas = n['h1'].popen(f"iperf -c 10.0.0.4 -t 5  -i 0.5 -p 3000 -Z vegas > client_h1_vegas.txt",shell=True)
-        h1_clinet_process_cubic.wait()
-        h1_clinet_process_reno.wait()
-        h1_clinet_process_bbr.wait()
-        h1_clinet_process_vegas.wait()
+        congestion_schemes = ['cubic', 'reno', 'bbr', 'vegas']
+        for scheme in congestion_schemes:
+            client_output_file = f"client_h1_{scheme}.txt"
+            client_process = n['h1'].popen(
+                f"iperf -c 10.0.0.4 -t 5 -i 0.5 -p 3000 -Z {scheme} > {client_output_file}",
+                shell=True)
 
     elif args.config == 'c':
         server_process = n['h4'].popen(f"iperf -s -t 5 -i 0.5 -p 3000 > server_file_output.txt", shell=True)
 
-        # Run client processes on H1, H2, H3 for each congestion control scheme
         congestion_schemes = ['cubic', 'reno', 'bbr', 'vegas']
         for h in ['h1', 'h2', 'h3']:
             for scheme in congestion_schemes:
@@ -80,12 +80,24 @@ def runTopo():
         
         client_process.wait()
         server_process.wait()
+
+    elif args.config == 'd':
+        server_process = n['h4'].popen(f"iperf -s -t 5 -i 0.5 -p 3000 > server_file_output.txt", shell=True)
+
+        congestion_schemes = ['cubic', 'reno', 'bbr', 'vegas']
+        link_losses = [0.01, 0.03]  # Add link loss values here
+
+        for link_loss in link_losses:
+                for scheme in congestion_schemes:
+                    client_output_file = f"client_h1_{scheme}_linkloss_{link_loss}.txt"
+                    client_process = n['h1'].popen(
+                        f"iperf -c 10.0.0.4 -t 5 -i 0.5 -p 3000 -Z {scheme} --link-loss {link_loss} > {client_output_file}",
+                        shell=True)
+
     CLI(n)
     n.stop()
 
 def main():
-    # congestions = ['cubic','reno', 'bbr', 'vegas']
-    # for congestion in congestions:
     runTopo()
 
 main()
